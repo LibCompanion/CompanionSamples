bcfbf3875db3308147fa0a77ba5b657a99bf1e20
Andreas Sekulski <andreas.sekulski@gmail.com>
2019-07-16 22:08:08 +0200

    Update cuda sample

--- FeatureMatching/CudaFeatureMatchingVideo.cpp
+++ FeatureMatching/CudaFeatureMatchingVideo.cpp
@@ -43,7 +43,6 @@ int main()
     std::string testVideo = VIDEO_EXAMPLE_PATH;
 
     // -------------- Setup used processing algo. --------------
-    Companion::Configuration *companion = new Companion::Configuration();
     int type = cv::DescriptorMatcher::FLANNBASED;
     cv::Ptr<cv::DescriptorMatcher> matcher = cv::DescriptorMatcher::create(type);
 
@@ -51,44 +50,49 @@ int main()
     cv::Ptr<cv::cuda::ORB> feature = cv::cuda::ORB::create(6000);
     feature->setBlurForDescriptor(true);
 
-    Companion::Algorithm::Recognition::Matching::Matching *matching = new Companion::Algorithm::Recognition::Matching::FeatureMatching(feature, 10, 40);
+	try
+	{
+		PTR_MATCHING_RECOGNITION matching = std::make_shared<FEATURE_MATCHING>(feature);
+		PTR_MATCH_RECOGNITION recognition = std::make_shared<MATCH_RECOGNITION>(matching, Companion::SCALING::SCALE_640x360);
 
-    // -------------- Image Processing Setup --------------
-    Companion::Processing::Recognition::MatchRecognition* recognition = new Companion::Processing::Recognition::MatchRecognition(matching, Companion::SCALING::SCALE_640x360);
-    companion->setProcessing(recognition);
-    companion->setSkipFrame(0);
-    companion->setResultHandler(resultHandler);
-    companion->setErrorHandler(errorHandler);
+		std::unique_ptr<COMPANION> companion = std::make_unique<COMPANION>();
+		companion->Processing(recognition);
+		companion->SkipFrame(0);
+		companion->ResultCallback(resultHandler);
+		companion->ErrorCallback(errorHandler);
 
-    // Setup video source to obtain images.
-    Companion::Input::Stream *stream = new Companion::Input::Video(testVideo);
+		// Setup video source to obtain images.
+		PTR_VIDEO_STREAM stream = std::make_shared<VIDEO_STREAM>(testVideo);
+		// Set input source
+		companion->Source(stream);
 
-    // Set input source
-    companion->setSource(stream);
+		// Store all searched data models
+		for (int i = 0; i < images.size(); i++)
+		{
+			PTR_MODEL_FEATURE_MATCHING model = std::make_shared<MODEL_FEATURE_MATCHING>();
+			model->ID(i);
+			model->Image(cv::imread(images[i], cv::IMREAD_GRAYSCALE));
 
-    // Store all searched data models
-    Companion::Model::Processing::FeatureMatchingModel *model;
-    for (int i = 0; i < images.size(); i++) 
-    {
-        model = new Companion::Model::Processing::FeatureMatchingModel();
-        model->setID(i);
-        model->setImage(cv::imread(images[i], cv::IMREAD_GRAYSCALE));
+			if (!recognition->AddModel(model))
+			{
+				std::cout << "Model not added";
+			}
+		}
 
-        if (!recognition->addModel(model))
-        {
-        std::cout << "Model not added";
-        }
-    }
-
-    // Execute companion
-    try 
-    {
-        companion->run();
-    }
-    catch (Companion::Error::Code errorCode) 
-    {
-        errorHandler(errorCode);
-    }
+		// Execute companion
+		try
+		{
+			companion->Run();
+		}
+		catch (Companion::Error::Code errorCode)
+		{
+			errorHandler(errorCode);
+		}
+	}
+	catch (cv::Exception ex)
+	{
+		std::cout << ex.msg;
+	}
 
     return 0;
 }
